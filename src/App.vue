4329a67b89532e7498ed1030a84882bcdf57ebc4
ecki-d <support@kaizenmedia.co.za>
2019-04-05 09:13:46 +0200

    initialize app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <!-- Material form contact -->
+  <mdb-container>
+    <mdb-row>
+      <mdb-col class="col-lg-6 col-md-8 col-sm-12 mx-auto">
+        <form @submit.prevent="handleSubmit" name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true" class="w-100">
+          <p style="display: none;" class="hidden">
+            <label>Don’t fill this out if you're human: <input name="bot-field" /></label>
+          </p>
+          <p class="h4 text-center mb-4">Write to us</p>
+          <div class="grey-text">
+            <mdb-input v-model="form.name" name="name" label="Your name" icon="user" group type="text" validate error="wrong" success="right"/>
+            <mdb-input v-model="form.email" name="email" label="Your email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+            <mdb-input v-model="form.subject" name="subject" label="Subject" icon="tag" group type="text" validate error="wrong" success="right"/>
+            <mdb-textarea v-model="form.message" name="message" :rows="2" label="Your message" icon="pencil"/>
+          </div>
+          <div class="text-center">
+            <mdb-btn outline="secondary">Send <mdb-icon far icon="paper-plane" class="ml-1"/></mdb-btn>
+          </div>
+        </form>
+      </mdb-col>
+    </mdb-row>
+  </mdb-container>
+  <!-- Material form contact -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { 
+    mdbInput, 
+    mdbBtn, 
+    mdbContainer, 
+    mdbRow, 
+    mdbCol, 
+    mdbTextarea,
+    mdbIcon
+  } from 'mdbvue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Basic',
+    data: () => ({
+      form: {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      }
+    }),
+    components: {
+      mdbInput,
+      mdbBtn,
+      mdbTextarea,
+      mdbContainer,
+      mdbRow,
+      mdbCol,
+      mdbTextarea
+    },
+    methods: {
+      encode(data) {
+        return Object.keys(data)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+        .join('&')
+      },
+      handleSubmit() {
+        fetch('/', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/x-www-urlencoded'
+          },
+          body: this.encode({
+            'form-name': 'contact',
+            ...this.form
+          })
+        })
+        .then(() => console.log('successfully sent'))
+        .catch(e => console.error(e))
+      }
+    }
   }
-}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
